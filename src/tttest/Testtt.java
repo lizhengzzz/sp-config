5f70a9fcf3a89d1366e4d4cf3e5aef3ddeec1258
63475 <634759864@qq.com>
2022-04-09 20:08:24 +0800

    1

---
+++ src/tttest/Testtt.java
@@ -0,0 +1,15 @@
+package tttest;
+
+/**
+ * @author lizheng
+ * @date 2022/4/9  20:07
+ */
+public class Testtt {
+    
+    public static void main (String[] args) {
+    
+        System.out.println("...");
+        //123
+    }
+    
+}
